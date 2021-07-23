1cbdd070b7b9f0145ee0e49448fb8ee9f4e76b39
Deng Pan <dpan968@gmail.com>
2021-07-23 22:29:54 +1000

    refractering

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import json
 from collections import defaultdict
 import pprint
 import copy
+import view.menu_enum as menu
 
+user_path = 'data/users.json'
+ticket_path = 'data/tickets.json'
 
 
 # init()
@@ -35,7 +38,7 @@ def add_in_ticket_field_to_user_list(user_list, ticket_list):
 
     return user_list_with_ticket_subject
 
-def create_ticket_table(user_list_dict, ticket_list):
+def add_assignee_name_to_ticket_list(user_list_dict, ticket_list):
     # add in the assignee_name field into the dictionary version of ticket_search_table
     ticket_list_with_assignee_name = copy.deepcopy(ticket_list)
     for ticket in ticket_list_with_assignee_name:
@@ -53,9 +56,7 @@ def convert_list_to_dict(item_list, key):
 
 
 
-
-
-def create_mapping_ticket_to_user(user, ticket):
+def init_game():
     pass
 
 
@@ -73,29 +74,32 @@ def search_by_field(item_search_table, item_list_indexed, search_term, search_va
 
 
 def create_index_by_field(item_list, field):
+
     search_table_by_field = defaultdict(list)
-    for item in item_list:
-        try:
-            search_table_by_field[item[field]].append(item['_id'])
-        except KeyError:
-            search_table_by_field['missing_field'].append((item['_id']))
-        except Exception as e:
-            print(e)
-            raise
+
+    if field == 'tags':
+        for item in item_list:
+            try:
+                for tag in item['tags']:
+                    search_table_by_field[tag].append(item['_id'])
+            except KeyError:
+                search_table_by_field['missing_field'].append((item['_id']))
+            except Exception as e:
+                print(e)
+                raise
+    else:
+        for item in item_list:
+            try:
+                search_table_by_field[item[field]].append(item['_id'])
+            except KeyError:
+                search_table_by_field['missing_field'].append((item['_id']))
+            except Exception as e:
+                print(e)
+                raise
     return search_table_by_field
 
-def create_index_by_tag(ticket_list, tag_list):
-    search_table_by_field = defaultdict(list)
-    for ticket in ticket_list:
-        try:
-            for tag in ticket['tags']:
-                search_table_by_field[tag].append(ticket['_id'])
-        except KeyError:
-            search_table_by_field['missing_field'].append((ticket['_id']))
-        except Exception as e:
-            print(e)
-            raise
-    return search_table_by_field
+
+
 
 def pretty_print_user(user_ids, user_search_table):
     for user_id in user_ids:
@@ -116,20 +120,6 @@ def ask_user_input():
         print("* Press 2 to view a list of searchable fields")
         print("* Type 'quit' to exit")
 
-def start_search(user_searchable_fields, ticket_searchable_fields):
-    search_entity = input("Select 1) Users or 2) Tickets\n")
-    if search_entity == "1":
-        entity = "user"
-    elif search_entity == "2":
-        entity = "ticket"
-    search_term = input("Enter search term\n")
-    if entity == 'user' and search_term in user_searchable_fields:
-        pass
-    elif entity == 'ticket' and search_term in ticket_searchable_fields:
-        pass
-    else:
-        input('Wrong searchable field, please double check.\n')
-
 
 
 def get_searchable_fields(item_list):
@@ -147,98 +137,80 @@ def list_searchable_fields(searchable_fields, entity_name):
     print(f"Search {entity_name} with")
     print(*searchable_fields, sep="\n")
 
+def init():
+    user_list = load_input_file(user_path)
+    ticket_list = load_input_file(ticket_path)
 
+
+    user_list_with_ticket_subject = add_in_ticket_field_to_user_list(user_list, ticket_list)
+    user_search_table = convert_list_to_dict(user_list_with_ticket_subject, '_id')
+    user_search_table_indexed_by_name = create_index_by_field(user_list_with_ticket_subject, 'name')
+    user_search_table_indexed_by_created_at = create_index_by_field(user_list, 'created_at')
+    user_search_table_indexed_by_verified = create_index_by_field(user_list, 'verified')
+
+    ticket_list_with_assignee_name = add_assignee_name_to_ticket_list(user_search_table, ticket_list)
+    ticket_search_table = convert_list_to_dict(ticket_list_with_assignee_name, '_id')
+    ticket_search_table_indexed_by_created_at = create_index_by_field(ticket_list, 'created_at')
+    ticket_search_table_indexed_by_type = create_index_by_field(ticket_list, 'type')
+    ticket_search_table_indexed_by_subject = create_index_by_field(ticket_list, 'subject')
+    ticket_search_table_indexed_by_assignee_id = create_index_by_field(ticket_list, 'assignee_id')
+    ticket_search_table_indexed_by_tag = create_index_by_field(ticket_list, 'tags')
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    user_path = 'data/users.json'
-    ticket_path = 'data/tickets.json'
-    user_list = load_input_file(user_path)
-    ticket_list = load_input_file(ticket_path)
+    init()
 
+    print(menu.WELCOME)
+    print(menu.SELECT_OPTIONS)
 
-    # get the user_search_table ready
-    user_search_table_list = add_in_ticket_field_to_user_list(user_list, ticket_list)
-
-
-    user_search_table = convert_list_to_dict(user_list, '_id')
-    user_search_table_indexed_by_name = create_index_by_field(user_list, 'name')
-    user_search_table_indexed_by_created_at = create_index_by_field(user_list, 'created_at')
-    user_search_table_indexed_by_verified = create_index_by_field(user_list, 'verified')
-
-    ticket_search_table_list = create_ticket_table(user_search_table, ticket_list)
-    ticket_search_table = convert_list_to_dict(ticket_search_table_list, '_id')
-
-    ticket_search_table_indexed_by_created_at = create_index_by_field(ticket_list, 'created_at')
-    ticket_search_table_indexed_by_type = create_index_by_field(ticket_list, 'type')
-    # why would I have missing_field here?????
-    ticket_search_table_indexed_by_subject = create_index_by_field(ticket_list, 'subject')
-    ticket_search_table_indexed_by_assignee_id = create_index_by_field(ticket_list, 'assignee_id')
-    ticket_search_table_indexed_by_tag = create_index_by_tag(ticket_list, 'tags')
-
-    WELCOME = "Welcome to Zendesk Search \n Type 'quit' to exit at any time, Press 'Enter' to continue\n"
-    SELECT_OPTIONS = "Select search option: \n * Press 1 to search Zendesk \n * Press 2 to view a list of searchable fields\n " \
-                     "* Type 'quit' to exit\n"
-    USER_SEARCHABLE_FIELD = get_searchable_fields(user_list)
-    TICKET_SEARCHABLE_FIELD = get_searchable_fields(ticket_list)
-
-    input_1 = input(WELCOME)
-    if input_1.lower() == 'quit':
-        exit()
-    input_2 = input(SELECT_OPTIONS).lower()
-    while input_2 != 'quit':
-
-        if input_2 == '1':
-
-            search_entity = input("Select 1) Users or 2) Tickets\n")
-            if search_entity == "1":
-                entity = "user"
-            elif search_entity == "2":
-                entity = "ticket"
-            search_term = input("Enter search term\n")
-            # TODO: need to validate the search_term
-            search_value = input('Enter search value\n')
-            # TODO: need to validate the search_value
-
-            print(f'Searching {entity} for {search_term} with a value of {search_value}\n')
-            # if entity == 'user' and search_term in USER_SEARCHABLE_FIELD:
-            #
-            #     if search_term == '_id':
-            #         print(search_by_id(user_search_table, int(search_value)))
-            #     else:
-            #         pprint.pprint(search_by_field(user_search_table, user_search_table_indexed_by_name, search_term, search_value))
-            # elif entity == 'ticket' and search_term in TICKET_SEARCHABLE_FIELD:
-            #     pass
-            # else:
-            #     input('Wrong searchable field, please double check.\n')
-
-
-
-        elif input_2 == '2':
-            list_searchable_fields(USER_SEARCHABLE_FIELD, 'User')
-            list_searchable_fields(TICKET_SEARCHABLE_FIELD, 'Ticket')
-            input_2 = input('\nPress 1 to search, type quit to exit the program\n')
-        else:
-            input_2 = input('Invalid value, please double check')
-
-
-
-
-
-
-
-    # list_searchable_fields(user_list, "User")
-    # list_searchable_fields(ticket_list, "Ticket")
-
-
-
-
-
-
-    # print(SELECT_OPTIONS)
+    # USER_SEARCHABLE_FIELD = get_searchable_fields(user_list)
+    # TICKET_SEARCHABLE_FIELD = get_searchable_fields(ticket_list)
+    #
+    # input_1 = input(WELCOME)
+    # if input_1.lower() == 'quit':
+    #     exit()
+    # input_2 = input(SELECT_OPTIONS).lower()
+    # while input_2 != 'quit':
+    #
+    #     if input_2 == '1':
+    #
+    #         search_entity = input("Select 1) Users or 2) Tickets\n")
+    #         if search_entity == "1":
+    #             entity = "user"
+    #         elif search_entity == "2":
+    #             entity = "ticket"
+    #         search_term = input("Enter search term\n")
+    #         # TODO: need to validate the search_term
+    #         search_value = input('Enter search value\n')
+    #         # TODO: need to validate the search_value
+    #
+    #         print(f'Searching {entity} for {search_term} with a value of {search_value}\n')
+    #         # if entity == 'user' and search_term in USER_SEARCHABLE_FIELD:
+    #         #
+    #         #     if search_term == '_id':
+    #         #         print(search_by_id(user_search_table, int(search_value)))
+    #         #     else:
+    #         #         pprint.pprint(search_by_field(user_search_table, user_search_table_indexed_by_name, search_term, search_value))
+    #         # elif entity == 'ticket' and search_term in TICKET_SEARCHABLE_FIELD:
+    #         #     pass
+    #         # else:
+    #         #     input('Wrong searchable field, please double check.\n')
+    #
+    #
+    #
+    #     elif input_2 == '2':
+    #         list_searchable_fields(USER_SEARCHABLE_FIELD, 'User')
+    #         list_searchable_fields(TICKET_SEARCHABLE_FIELD, 'Ticket')
+    #         input_2 = input('\nPress 1 to search, type quit to exit the program\n')
+    #     else:
+    #         input_2 = input('Invalid value, please double check')
+    #
+    #
+    #
+    #
 
 
 
--- view/menu_enum.py
+++ view/menu_enum.py
@@ -1,5 +1,4 @@
-from enum import Enum
-class Top_Level_Menu(Enum):
-    WELCOME = "Welcome to Zendesk Search \n Type 'quit' to exit at any time, Press 'Enter' to continue"
-    SELECT_OPTIONS = "Select search option: \n * Press 1 to search Zendesk \n Press 2 to view a list of searchable fields\n " \
-                     "* Type 'quit' to exit"
+
+WELCOME = "Welcome to Zendesk Search \n Type 'quit' to exit at any time, Press 'Enter' to continue\n"
+SELECT_OPTIONS = "Select search option: \n * Press 1 to search Zendesk \n * Press 2 to view a list of searchable fields\n " \
+                 "* Type 'quit' to exit\n"
